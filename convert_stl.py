10b11695c720adb03843535cec34476eadd04a1c
tomqwrtyu <lifeqwertyui@gmail.com>
2023-06-13 17:58:07 +0800

    230613 cup and camera model

--- convert_stl.py
+++ convert_stl.py
@@ -30,61 +30,42 @@ from vpython import *
 def stl_to_triangles(fileinfo): # specify file
     # Accept a file name or a file descriptor; make sure mode is 'rb' (read binary)
     fd = open(fileinfo, mode='rb')
-    text = fd.read()
     tris = [] # list of triangles to compound
-    if False: # prevent executing code for binary file
-        pass
-    # The following code for binary files must be updated:
-#    if chr(0) in text: # if binary file
-#        text = text[84:]
-#        L = len(text)
-#        N = 2*(L//25) # 25/2 floats per point: 4*3 float32's + 1 uint16
-#        triPos = []
-#        triNor = []
-#        n = i = 0
-#        while n < L:
-#            triNor[i] = fromstring(text[n:n+12], float32)
-#            triPos[i] = fromstring(text[n+12:n+24], float32)
-#            triPos[i+1] = fromstring(text[n+24:n+36], float32)
-#            triPos[i+2] = fromstring(text[n+36:n+48], float32)
-#            colors = fromstring(text[n+48:n+50], uint16)
-#            if colors != 0:
-#                print '%x' % colors
-#            if triNor[i].any():
-#                triNor[i] = triNor[i+1] = triNor[i+2] = norm(vector(triNor[i]))
-#            else:
-#                triNor[i] = triNor[i+1] = triNor[i+2] = \
-#                    norm(cross(triPos[i+1]-triPos[i],triPos[i+2]-triPos[i]))
-#            n += 50
-#            i += 3
-    else:
-        fd.seek(0)
-        fList = fd.readlines()
-    
-        # Decompose list into vertex positions and normals
-        vs = []
-        for line in fList:
-            FileLine = line.split( )
-            if FileLine[0] == b'facet':
-                N = vec(float(FileLine[2]), float(FileLine[3]), float(FileLine[4]))
-            elif FileLine[0] == b'vertex':
-                vs.append( vertex(pos=vec(float(FileLine[1]), float(FileLine[2]), float(FileLine[3])), normal=N, color=color.white) )
-                if len(vs) == 3:
-                    tris.append(triangle(vs=vs))
-                    vs = []
+    fd.seek(0)
+    fList = fd.readlines()
+
+    # Decompose list into vertex positions and normals
+    vs = []
+    for line in fList:
+        FileLine = line.split( )
+        if FileLine[0] == b'facet':
+            N = vec(float(FileLine[2]), float(FileLine[3]), float(FileLine[4]))
+        elif FileLine[0] == b'vertex':
+            vs.append( vertex(pos=vec(float(FileLine[1]), float(FileLine[2]), float(FileLine[3])), normal=N, color=color.white) )
+            if len(vs) == 3:
+                tris.append(triangle(vs=vs))
+                vs = []
                     
     return compound(tris)
 
 if __name__ == '__main__':
-    man = stl_to_triangles('model/Intel_RealSense_Depth_Camera_D435.stl')
-    man.size *= 10
+    scene = canvas(title = "CV term project", width = 1280, height = 720, x = 0, y = 0, center = vector(0, 0.1, 0), \
+                    background = vector(0, 0.6, 0.6))
+    size = 0.1   # 木塊邊長
+    L = 1        # 地板長度
+    v = 0.03     # 木塊速度
+    t = 0        # 時間
+    dt = 0.01    # 時間間隔
+    a_floor = box(pos = vector(0, 0, 0), length = L, height = size*0.1, width = L*0.5, color = color.blue)
+
+    man = stl_to_triangles("model/cupper.stl")
     man.pos = vec(-200,0,0)
     man.color = color.orange
     # part = stl_to_triangles('Part1.stl')
     # part.pos = vec(-200,0,0)
     # part.color = color.orange
-    cam = stl_to_triangles("model/cupper.stl")
-    # cam.size *= 200
+    cam = stl_to_triangles("model/Intel_RealSense_Depth_Camera_D435.stl")
+    cam.size *= 10
     cam.pos = vec(250,0,0)
     cam.color = color.blue
     for i in range(10):
